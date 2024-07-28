94d0777350d177e6a1e121d1452a0b585a98af4f
satyam00043 <satyam00043@gmail.com>
2024-07-28 17:12:50 +0530

    controoller

--- src/controller/user.controler.js
+++ src/controller/user.controler.js
@@ -1,6 +1,17 @@
 import asyncHandler from '../utils/asyncHandler.js'
 const registerUser=asyncHandler(async (req,res)=>{
 
+    //get user details
+    //validation/
+    //check username and email if user already exist
+    //ceck image and avatar;
+    //upload on the cloudenary
+    //save user details in database entry in details
+    //    remove password and response fiels from response field
+    //check for user creation  
+    const userobj=[
+        {userName:"satyam"}
+    ]
     res.status(200).json({
         message:"real world projext"
     })
